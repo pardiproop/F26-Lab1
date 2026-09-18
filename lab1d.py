8d48c4a3fcb79577426b8909dfa0d52d01a0e804
pardiproop <prooprai1@myseneca.ca>
2026-09-18 17:23:10 +0000

    Completed Lab1d in class

--- lab1d.py
+++ lab1d.py
@@ -2,8 +2,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author: 
-# Date:
+# Author: Pardip Rooprai
+# Date: 18/9/2026
 # Purpose: Use string methods and f-string formating.
 # Usage: python3 lab1d.py
 
@@ -12,6 +12,11 @@
 # Use the string method .upper() to convert the name to upper case.
 # Create another variable called “age”, the value of “age” should be your age
 # The script, when executed, should print out "How are you yourname? Happy xxth birthday!" To print this output use .format() method. 
+name="Pardip Rooprai"
+name=name.upper()
+age=18
+mystr="How are you {}? Happy {}th Birthday!".format(name, age)
+print(mystr)
 
 #TO-DO 2:
 # Create a variable called "words".
--- lab1e.py
+++ lab1e.py
@@ -2,8 +2,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Pardip Rooprai
+# Date: 18/9/2026
 # Purpose: Use string methods and f-string formating.
 # Usage: python3 lab1e.py
 
@@ -14,4 +14,12 @@
 # The value of "stock" should also be a decimal number of your own choice.
 # Print the product of `quantity` and `stock` with 4 spaces before the answer using the module % formatting.
 # Then print the product of `quantity` and `stock` with 7 spaces before the answer and make sure the answer only goes to hundreadths (-.--) using the module % formatting.
-
+quantity=3.5
+stock=7.95
+product=quantity*stock
+print("The product is %4d" %(product))
+print("The product is %7.2d" %(product))
+print("The product is %15.2d" %(product))
+print("The product is %4f" %(product))
+print("The product is %7.5f" %(product))
+print("The product is %15.5f" %(product))
