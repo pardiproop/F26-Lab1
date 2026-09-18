15ab21efe863a7c3c0f24e741244d6168d5455c1
pardiproop <prooprai1@myseneca.ca>
2026-09-18 16:26:19 +0000

    Completed Lab1a in class

--- lab1a.py
+++ lab1a.py
@@ -1,10 +1,20 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Pardip Rooprai
+# Date: 18/9/2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: python3 lab1a.py
+message="Welcome to PRG101" # this prints a welcome message to the user
+print(message) # this is the print message function/feature
+print(type(message)) # this prints the type of message
+newstring=message+"Python is Future" # 
+# print(message+"Python is Future")
+print(newstring)
+x=10
+print(type(x))
+x="hello"
+print(type(x))
 
 # TO DO 1: Creating and using varibales
 # create a variable called message.
